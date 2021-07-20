9ae6d2ea63ec997fea3c1edb238bbbe544e45f34
Dan Dobrin <dan.dobrin@gmail.com>
2021-07-20 11:20:31 -0400

    first commit

--- deploy/environment/kind/deploy.sh
+++ deploy/environment/kind/deploy.sh
@@ -27,17 +27,17 @@ start() {
     then
         IPADDRESS="$(hostname -I |grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])' |head -n 1)" # workaround if ifconfig is not installed on recent versions of Debian
     fi
-    INSECURE_REGISTRIES=""
-    for i in {1..9}
-    do
-    INSECURE_REGISTRIES+="  [plugins.\"io.containerd.grpc.v1.cri\".registry.mirrors.\"${WORKSHOP_NAME}-w01-s00${i}-registry.${IPADDRESS}.nip.io\"]"
-    INSECURE_REGISTRIES+="\n    endpoint = [\"http:\\/\\/${WORKSHOP_NAME}-w01-s00${i}-registry.${IPADDRESS}.nip.io\"]\n"
-    done
+    # INSECURE_REGISTRIES=""
+    # for i in {1..9}
+    # do
+    # INSECURE_REGISTRIES+="  [plugins.\"io.containerd.grpc.v1.cri\".registry.mirrors.\"${WORKSHOP_NAME}-w01-s00${i}-registry.${IPADDRESS}.nip.io\"]"
+    # INSECURE_REGISTRIES+="\n    endpoint = [\"http:\\/\\/${WORKSHOP_NAME}-w01-s00${i}-registry.${IPADDRESS}.nip.io\"]\n"
+    # done
 
-    sed "s/  #__EXTRA_REGISTRIES__/${INSECURE_REGISTRIES}/g" ${DIR}/kind-config.yaml > ${DIR}/kind-config.yaml.resolved
+    # sed "s/  #__EXTRA_REGISTRIES__/${INSECURE_REGISTRIES}/g" ${DIR}/kind-config.yaml > ${DIR}/kind-config.yaml.resolved
 
     echo "===== Creating cluster"
-    kind create cluster --name "${CLUSTER_NAME}" --config=$DIR/kind-config.yaml.resolved
+    kind create cluster --name "${CLUSTER_NAME}" --config=$DIR/kind-config.yaml
     rm ${DIR}/kind-config.yaml.resolved
 
     # connect the registry to the cluster network
